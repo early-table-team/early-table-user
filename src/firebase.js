c8dad17441e21409ef29544fd76403e693f4cc0c
LYJ <lyj.blessed2@gmail.com>
2025-02-04 10:21:35 +0900

    Feat: Add FCM notification

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
+// Firebase 설정 객체 (콘솔에서 복사한 내용)
+const firebaseConfig = {
+    apiKey: "AIzaSyBCHjGjYjeHJ_BfnYr6971MNAmLmedWKa8",
+    authDomain: "early-table-6315a.firebaseapp.com",
+    projectId: "early-table-6315a",
+    storageBucket: "early-table-6315a.firebasestorage.app",
+    messagingSenderId: "531693985268",
+    appId: "1:531693985268:web:404dd20c73d5d90e416da1",
+    measurementId: "G-2K9VLFMZK3"
+};
+
+// Firebase 앱 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase Cloud Messaging 초기화
+const messaging = getMessaging(app);
+
+export { messaging };
